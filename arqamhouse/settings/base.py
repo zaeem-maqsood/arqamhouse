17b90c1d2660c160aad2ce7c8bbce3d09b14fb35
Zaeem Maqsood <zaeem.maqsood@gmail.com>
2019-08-26 16:21:19 -0400

    Django-heroku

--- arqamhouse/settings/base.py
+++ arqamhouse/settings/base.py
@@ -10,12 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import django_heroku
+# import django_heroku
 import os
 from celery.schedules import crontab
 from arqamhouse.aws.conf import *
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,9 +117,9 @@ DATABASES = {
 #     }
 # }
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
 
 
