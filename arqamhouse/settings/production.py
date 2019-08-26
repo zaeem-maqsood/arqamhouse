ec891b46bb1fcf6b657910c35f89490dde7579c2
Zaeem Maqsood <zaeem.maqsood@gmail.com>
2019-08-26 17:22:57 -0400

    Test

--- arqamhouse/settings/production.py
+++ arqamhouse/settings/production.py
@@ -10,5 +10,5 @@ STRIPE_PUBLIC_KEY = 'pk_live_HV9p4w8uuKQelYHckqDC1m0d'
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
+del DATABASES['default']['OPTIONS']['sslmode']
 # django_heroku.settings(locals())
