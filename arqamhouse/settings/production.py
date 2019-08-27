21ed93aaf27ef8c14bdcbb5f09212ed89db338bf
Zaeem Maqsood <zaeem.maqsood@gmail.com>
2019-08-27 10:53:42 -0400

    SSL

--- arqamhouse/settings/production.py
+++ arqamhouse/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['arqam.herokuapp.com', ]
+ALLOWED_HOSTS = ['arqam.herokuapp.com', 'arqamhouse.com', 'www.arqamhouse.com' ]
 
 STRIPE_SECRET_KEY = 'sk_live_4FvrAHAiKVjvUYouYDUAuT63'
 STRIPE_PUBLIC_KEY = 'pk_live_HV9p4w8uuKQelYHckqDC1m0d'
@@ -10,5 +10,5 @@ STRIPE_PUBLIC_KEY = 'pk_live_HV9p4w8uuKQelYHckqDC1m0d'
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-del DATABASES['default']['OPTIONS']['sslmode']
+# del DATABASES['default']['OPTIONS']['sslmode']
 # django_heroku.settings(locals())
