9dc00181f82e9b0298828f8b5ef9cad58ed93db4
Zaeem Maqsood <zaeem.maqsood@gmail.com>
2019-09-26 16:16:15 -0400

    Added prod keys

--- arqamhouse/settings/production.py
+++ arqamhouse/settings/production.py
@@ -5,14 +5,14 @@ DEBUG = False
 ALLOWED_HOSTS = ['arqam.herokuapp.com', 'arqamhouse.com', 'www.arqamhouse.com' ]
 
 
-# Test Keys
-STRIPE_SECRET_KEY = 'sk_test_4o8iCn4HF5nUwtaVsnc3ysRh'
-STRIPE_PUBLIC_KEY = 'pk_test_kqPiLNskd3LohLMfI49hnwzf'
+# # Test Keys
+# STRIPE_SECRET_KEY = 'sk_test_4o8iCn4HF5nUwtaVsnc3ysRh'
+# STRIPE_PUBLIC_KEY = 'pk_test_kqPiLNskd3LohLMfI49hnwzf'
 
 
-# Production Keys
-# STRIPE_SECRET_KEY = 'sk_live_4FvrAHAiKVjvUYouYDUAuT63'
-# STRIPE_PUBLIC_KEY = 'pk_live_HV9p4w8uuKQelYHckqDC1m0d'
+Production Keys
+STRIPE_SECRET_KEY = 'sk_live_4FvrAHAiKVjvUYouYDUAuT63'
+STRIPE_PUBLIC_KEY = 'pk_live_HV9p4w8uuKQelYHckqDC1m0d'
 
 
 DATABASES = {}
