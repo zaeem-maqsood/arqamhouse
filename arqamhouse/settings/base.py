04ab99dcb709a7b75291bacfacabf679e23c5a1d
Zaeem Maqsood <zaeem.maqsood@gmail.com>
2019-11-01 16:32:58 -0400

    Removed redis url

--- arqamhouse/settings/base.py
+++ arqamhouse/settings/base.py
@@ -104,8 +104,6 @@ CITIES_LIGHT_INCLUDE_CITY_TYPES = ['PPL', 'PPLA', 'PPLA2', 'PPLA3', 'PPLA4', 'PP
 
 
 # Celery stuff 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
