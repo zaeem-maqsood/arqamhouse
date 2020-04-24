8be66eb93b04d99d0b4ea93fffbcd3644231cdd5
Zaeem Maqsood <zaeem.maqsood@gmail.com>
2020-04-24 11:02:13 -0400

    Fix

--- events/models/resources.py
+++ events/models/resources.py
@@ -1,14 +1,11 @@
-
-
 from .base import *
+import os
 from events.models import Event
 from core.models import TimestampedModel
-
+from django.core.exceptions import ValidationError
 
 
 def validate_file_extension(value):
-    import os
-    from django.core.exceptions import ValidationError
     ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
     valid_extensions = ['.pdf', '.doc', '.docx', '.jpg', '.png', '.xlsx', '.xls']
     if not ext.lower() in valid_extensions:
