86be73e44710117b5ad14a3ddb8eebbba81f46e2
Zaeem Maqsood <zaeem.maqsood@gmail.com>
2020-09-29 19:37:32 -0400

    Admin

--- postcards/admin.py
+++ postcards/admin.py
@@ -4,8 +4,8 @@ from postcards.models import PostCard, PostCardOrder
 
 
 class PostcardOrderAdmin(admin.ModelAdmin):
-    list_display = ('sent_to_recipient', 'post_card', 'name', 'recipient_name',
-                    'message_to_recipient', 'recipient_address', 'recipient_postal_code', 'anonymous')
+    list_display = ('name', 'sent_to_recipient', 'post_card', 'recipient_name',
+                    'message_to_recipient', 'recipient_address', 'recipient_postal_code', 'anonymous', 'postal_code')
     list_filter = ('post_card',)
     search_fields = ('recipient_name', 'name')
 
